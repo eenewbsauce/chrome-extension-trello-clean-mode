b480c4db0e99b1f4585adaef8e87ab9c53908c62
eenewbsauce <ryan.m.tracey@gmail.com>
2017-05-24 15:49:46 -0400

    feat(reset settings): cook that up

--- options.js
+++ options.js
@@ -4,14 +4,45 @@ window.addEventListener('load', function (evt) {
   $('#stickers-checkbox').on('click', toggle);
   $('#badges-checkbox').on('click', toggle);
   $('#cover-images-checkbox').on('click', toggle);
-  
+
   $('#clear').on('click', clearStorage);
 });
+
+function getInitialSettings() {
+  var baseSettings = {
+    members: {
+      selector: '.list-card-members'
+    },
+    labels: {
+      selector: '.list-card-labels'
+    },
+    stickers: {
+      selector: '.list-card-stickers-area'
+    },
+    badges: {
+      selector: '.badges'
+    },
+    "cover-images": {
+      selector: '.list-card-cover'
+    }
+  }
+
+  for(var key in baseSettings) {
+    var setting = baseSettings[key];
+    setting.isChecked = true;
+  }
+
+  return baseSettings;
+}
 
 setupOptions();
 
 function clearStorage() {
   chrome.storage.sync.clear();
+  setupOptions();
+  // $('input[type="checkbox"]').each((i, elem) => {
+  //   $(elem).prop('checked', true);
+  // })
 }
 
 function toggle() {
@@ -20,7 +51,7 @@ function toggle() {
   var isChecked = $elem.prop('checked');
 
   sendToggleMessage({
-    element: $elem.attr('name'),
+    name: $elem.attr('name'),
     isChecked: isChecked,
     selector: selector
   }, true);
@@ -31,8 +62,7 @@ function sendToggleMessage(message, saveToStorage) {
     chrome.tabs.sendMessage(tab.id, message, function(response) {
       if (saveToStorage) {
         chrome.storage.sync.set({
-          [message.element]: {
-            name: message.element,
+          [message.name]: {
             isChecked: message.isChecked,
             selector: message.selector
           }
@@ -44,6 +74,11 @@ function sendToggleMessage(message, saveToStorage) {
 
 function setupOptions() {
   chrome.storage.sync.get(function(options) {
+    if (Object.keys(options).length === 0) {
+      options = getInitialSettings();
+      chrome.storage.sync.set(options, () => {})
+    }
+
     for (var key in options) {
       $(`#${key}-checkbox`).prop('checked', options[key].isChecked);
       sendToggleMessage(createMessageFromStorage(options[key]), false);
@@ -52,5 +87,5 @@ function setupOptions() {
 }
 
 function createMessageFromStorage(storageItem) {
-  return { element: storageItem.name, isChecked: storageItem.isChecked, selector: storageItem.selector };
+  return { isChecked: storageItem.isChecked, selector: storageItem.selector };
 }
